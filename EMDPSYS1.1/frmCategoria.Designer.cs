3f52a30fb9675ee3a0db0a165ad49b78cb60c9af
Daniel Bravo Alvarez Calderon <62306343+danielbravoac@users.noreply.github.com>
2020-08-20 16:24:37 -0500

    Initial commit

---
+++ EMDPSYS1.1/frmCategoria.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace EMDPSYS1._1
+{
+    partial class frmCategoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmCategoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 480);
+            this.Name = "frmCategoria";
+            this.Text = "Categoria";
+            this.Load += new System.EventHandler(this.frmCategoria_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
